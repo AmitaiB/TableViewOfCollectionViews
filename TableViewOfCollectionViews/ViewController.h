76633417ebe367f57d2af2365a08b3896ed993eb
AmitaiB <blickstein@gmail.com>
2015-09-24 21:38:08 -0400

    Initial commit

---
+++ TableViewOfCollectionViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewOfCollectionViews
+//
+//  Created by Amitai Blickstein on 9/24/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewOfCollectionViews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewOfCollectionViews
+//
+//  Created by Amitai Blickstein on 9/24/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
